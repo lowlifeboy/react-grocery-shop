3a8875cfdae29894342def5d3a2fdb3007643f29
Konstantin Savchenko <killerkennyx41@gmail.com>
2020-01-11 00:20:57 +0200

    Updates

---
+++ precache-manifest.ffb06f0302104809013492fc1b8e8a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec4502845ba3d99b9ff1adb91c10d64",
+    "url": "/react-grocery-shop/index.html"
+  },
+  {
+    "revision": "dc755ea26c6629f5f4f1",
+    "url": "/react-grocery-shop/static/css/main.28a16d04.chunk.css"
+  },
+  {
+    "revision": "37920f51d8485dec8ad1",
+    "url": "/react-grocery-shop/static/js/2.cc717328.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-grocery-shop/static/js/2.cc717328.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc755ea26c6629f5f4f1",
+    "url": "/react-grocery-shop/static/js/main.e8961af9.chunk.js"
+  },
+  {
+    "revision": "4b7c872cfbecdac11605",
+    "url": "/react-grocery-shop/static/js/runtime-main.9f5e72a4.js"
+  }
+]);
